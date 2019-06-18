abd092e46aac9aa433d639e9a29a56193c7fb623
Daniel Schneider <daschne8@gmail.com>
2019-06-18 15:46:38 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,6 +2,10 @@ function paragraphSelector(){
   return $('p')
 }
 function lastImageSelector(){
-  let images =  $('img')
-  return images[images.length-1]
+  // let images =  $('img')
+  // return images[images.length-1]
+  return $('img:last-child')
+}
+function functionName() {
+  
 }
